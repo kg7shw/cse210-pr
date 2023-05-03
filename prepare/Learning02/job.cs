a6da4d2c5731b32eccc32ea8b833573c688aaf78
kg7shw <kg7shw@gmail.com>
2023-05-02 21:02:39 -0600

    added display to program.cs

---
+++ prepare/Learning02/job.cs
@@ -0,0 +1,15 @@
+public class Job
+{
+   public string _jobTitle;
+   public string _company;
+
+   public int _startYear;
+
+   public int _endYear;
+
+
+public void Display()
+{
+    Console.WriteLine($"Job Title: {_jobTitle} {_company} {_startYear}-{_endYear}");
+}
+}

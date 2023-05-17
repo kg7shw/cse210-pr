e8248f3f6456cd954dfd2481efab47eb279d716e
kg7shw <kg7shw@gmail.com>
2023-05-17 10:01:02 -0600

    edited the menu

--- prove/Develop02/Menu.cs
+++ prove/Develop02/Menu.cs
@@ -1,145 +1,168 @@
+using System.IO; 
+
 public class Menu
 {
     private Journal _journal;
+    private Prompt _prompt;
+    private string _fileName = "";
+    private string new_prompt = "";
 
-    public Menu(Journal journal)
+    public Menu(Journal journal, Prompt prompt, string fileName)
     {
         _journal = journal;
+        _prompt = prompt;
+        _fileName = fileName;
     }
 
     public void Display()
     {
         string response = "";
-        string[] menu_options = {"L","A","D","Q","F","S"};
+        string[] menu_options = {"L", "S", "Q"};
         while(response != "Q")
         {
             while(menu_options.Contains(response)==false)
             {
-                Console.Write("[L]oad journal[A]dd entry:\n[D]isplay entries\n[F]ind entries\n[Q]uit\n\nWhat do you want to do? ");
-
+                Console.WriteLine("[L]oad journal: ");
+                Console.WriteLine("[S]ave journal: ");
+                Console.WriteLine("[Q]uit");
+                Console.Write("What do you want to do? ");
+                
                 response = Console.ReadLine() ?? String.Empty;
                 response = response.ToUpper();
+                
 
             }
+            
 
             switch (response)
             {
+
                 case "Q":
                     Environment.Exit(0);
                     break;
-                case "A":
-                    Console.Write("[G]et random Prompt and add a journal entry");
-                    Console.Write("[W]rite in your journal without a prompt");
-                    Console.Write("[A]dd your own prompt and write a journal entry");
-                    Console.Write("\nWhat would you like to do? ");
+                case "S":
+                Console.WriteLine("Enter the filename to save the journal (without extension):");
+                _fileName = Console.ReadLine() ?? String.Empty;
+                    _journal.SaveJournalToFile();
+                    break;
+                
+                case "L":
+                    Console.Write("Enter the filename to load the journal from (including extension): ");
+                    
+                    _fileName = Console.ReadLine()?.ToUpper() ?? String.Empty;
+                        // Load the existing file
+                        _journal.LoadJournalFromFile(_fileName);
+                    
+                    // User selects another option.
+                    Console.WriteLine("[A]dd entry: ");
+                    Console.WriteLine("[D]isplay entries: ");
+                    Console.WriteLine("[F]ind entries: ");
+                    Console.Write("What do you want to do? ");
 
-                    string A_SubResponse = "";
-                    A_SubResponse = Console.ReadLine() ?? String.Empty;
-                    A_SubResponse = A_SubResponse.ToUpper();
-                    string[] SubMenu_options = {"G","W","A"};
-
-                    switch (A_SubResponse)
+                    string L_SubMenu_choice = "";
+                    L_SubMenu_choice = Console.ReadLine()?.ToUpper() ?? String.Empty;
+                    string[] L_SubMenu_options = {"A", "D", "F"};
+                    
+                
+                    switch (L_SubMenu_choice)
                     {
-                        case "G":
-                            // GetRandomPrompt()
-                            Console.Write("Please enter your entry: ");
-                            string entry = Console.ReadLine() ?? String.Empty;
-                            Console.Write("Please enter your title: ");
-                            string title = Console.ReadLine() ?? String.Empty;
-                            Console.Write("Please enter your author: ");
-                            string author = Console.ReadLine() ?? String.Empty;
-
-                            _journal.AddEntry(new Entry(entry, title, author));
-                            break;
                         case "A":
+                            Console.WriteLine("[G]et random Prompt and add a journal entry");
+                            Console.WriteLine("[W]rite in your journal without a prompt");
+                            Console.WriteLine("[A]dd your own prompt and write a journal entry");
+                            Console.Write("\nWhat would you like to do? ");
 
-                            // AddPrompt()
-                            Console.Write("Please enter your entry: ");
-                            entry = Console.ReadLine() ?? String.Empty;
-                            Console.Write("Please enter your title: ");
-                            title = Console.ReadLine() ?? String.Empty;
-                            Console.Write("Please enter your author: ");
-                            author = Console.ReadLine() ?? String.Empty;
+                            string A_SubResponse = "";
+                            A_SubResponse = Console.ReadLine() ?? String.Empty;
+                            A_SubResponse = A_SubResponse.ToUpper();
+                            string[] SubMenu_options = {"G","W","A"};
 
-                            _journal.AddEntry(new Entry(entry, title, author));
+                            switch (A_SubResponse)
+                            {
+                                case "G":
+                                    // GetRandomPrompt()
+
+                                   new_prompt = _journal.GetRandomPrompt();
+                                    Console.Write("Please enter who is writing the Journal entry: ");
+                                    string author = Console.ReadLine() ?? String.Empty;
+                                    Console.Write("Please enter the title of your entry: ");
+                                    string title = Console.ReadLine() ?? String.Empty;
+                                    Console.Write("Please enter your entry: ");
+                                    string entry = Console.ReadLine() ?? String.Empty;
+
+                                    _journal.AddEntry(new Entry(new_prompt, author, title, entry));
+                                    break;
+                                case "A":
+
+                                    // AddPrompt()
+                                    Console.Write("Please enter the prompt you would like to add: ");
+                            
+                                    new_prompt = Console.ReadLine() ?? String.Empty;
+                                    _prompt.AddPrompt(new_prompt);
+                                    Console.Write("Please enter your entry: ");
+                                    entry = Console.ReadLine() ?? String.Empty;
+                                    Console.Write("Please enter your title: ");
+                                    title = Console.ReadLine() ?? String.Empty;
+                                    Console.Write("Please enter your author: ");
+                                    author = Console.ReadLine() ?? String.Empty;
+
+                                    _journal.AddEntry(new Entry(new_prompt, entry, title, author));
+                                    break;
+                                case "W":
+                                    Console.Write("Please enter your entry: ");
+                                    entry = Console.ReadLine() ?? String.Empty;
+                                    Console.Write("Please enter your title: ");
+                                    title = Console.ReadLine() ?? String.Empty;
+                                    Console.Write("Please enter your author: ");
+                                    author = Console.ReadLine() ?? String.Empty;
+
+                                    _journal.AddEntry(new Entry(new_prompt, author, title, entry));
+                                    break;
+                            }
+
+                            
                             break;
-                        case "W":
-                            Console.Write("Please enter your entry: ");
-                            entry = Console.ReadLine() ?? String.Empty;
-                            Console.Write("Please enter your title: ");
-                            title = Console.ReadLine() ?? String.Empty;
-                            Console.Write("Please enter your author: ");
-                            author = Console.ReadLine() ?? String.Empty;
-
-                            _journal.AddEntry(new Entry(entry, title, author));
+                        case "S":
+                            _journal.ShowEntry();
                             break;
-                    }
+                        // case "F":
+                        //     Console.WriteLine("Search by: [T]itle");
+                        //     Console.WriteLine("Search by: [D]ate");
+                        //     Console.WriteLine("Search by: [A]uthor");
+                        //     Console.Write("\nWhat type of search do you want to perform? ");
+
+                        //     string searchType = "";
+                        //     searchType = Console.ReadLine() ?? String.Empty;
+                        //     searchType = searchType.ToUpper();
+                        //     string[] F_SubMenu_options = {"T","D","A"};
+
+                        //     switch (searchType)
+                        //     {
+                        //         case "T":
+
+
+                        //             break;
+                        //         case "D":
+
+
+                        //             break;
+                        //         case "A":
+                        //             // Console.Write("Please enter your author: ");
+                        //             // author = Console.ReadLine() ?? String.Empty;
+                        //             // Journal.FindEntriesByAuthor(author);
+                        //             break;
+                        //     }
+
 
                     
-                    break;
-                case "S":
-                    _journal.ShowEntry();
-                    break;
-                case "F":
-                    Console.Write("Search by: [T]itle");
-                    Console.Write("Search by: [D]ate");
-                    Console.Write("Search by: [A]uthor");
-                    Console.Write("\nWhat type of search do you want to perform? ");
-
-                    string searchType = "";
-                    searchType = Console.ReadLine() ?? String.Empty;
-                    searchType = searchType.ToUpper();
-                    string[] F_SubMenu_options = {"T","D","A"};
-
-                    switch (searchType)
-                    {
-                        case "T":
-
-
                             break;
-                        case "D":
 
-
+                        case "R":
+                            // _journal.GetRandomentry();
                             break;
-                        case "A":
-                            // Console.Write("Please enter your author: ");
-                            // author = Console.ReadLine() ?? String.Empty;
-                            // Journal.FindEntriesByAuthor(author);
-                            break;
-                    }
+                        }
+                    break;   
 
-
-                    // string searchTerm = Console.ReadLine() ?? "";
-                    // Console.WriteLine("[D]ate search");
-                    // Console.WriteLine("[T]opic search");
-                    // Console.WriteLine("[K]eywords search");
-                    // Console.Write("\nWhat type of search do you want to perform? ");
-                    // string searchType = Console.ReadLine()?.ToUpper() ?? "";
-                    // // if (searchType == "D")
-                    // // {
-                    // //     Console.Write("Please enter the date (e.g., yyyy-MM-dd): ");
-                    // //     string dateInput = Console.ReadLine() ?? "";
-                    // //     if (DateTime.TryParse(dateInput, out DateTime date))
-                    // //         _journal.FindEntriesByDate(date);
-                    // // }
-                    // // else if (searchType == "T")
-                    // // {
-                    // //     Console.Write("Please enter the topic: ");
-                    // //     string topic = Console.ReadLine() ?? "";
-                    // //     _journal.FindEntriesByTopic(topic);
-                    // // }
-                    // // else if (searchType == "K")
-                    // // {
-                    // //     Console.Write("Please enter the keywords (comma-separated): ");
-                    // //     string keywordsInput = Console.ReadLine() ?? "";
-                    // //     string[] keywords = keywordsInput.Split(',');
-                    // //     _journal.FindEntriesByKeywords(keywords);
-                    // }
-                    break;
-
-                case "R":
-                    // _journal.GetRandomentry();
-                    break;
 
                     
 

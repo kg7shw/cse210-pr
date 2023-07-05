069dd7a10685994a9078d157e518d2c32815b830
kg7shw <kg7shw@gmail.com>
2023-07-05 11:34:15 -0600

    added the meal class

--- Practice/Final_project/MealMaker/Meal.cs
+++ Practice/Final_project/MealMaker/Meal.cs
@@ -1,4 +1,40 @@
 public class Meal
 {
-    
+    private string _name;
+    private int _mealItems;
+
+    List<Recipe> _recipes = new List<Recipe>();
+
+
+
+
+    public Meal()
+    {
+        SetMealName();
+        CreateMeal();
+
+    }
+
+
+    public void SetMealName()
+    {
+        Console.Write("What is the name of this meal? ");
+        _name = Console.ReadLine() ?? String.Empty;
+    }
+    public void CreateMeal()
+    {
+        Console.Write("How many meal items are there in this meal? ");
+        _mealItems = int.Parse(Console.ReadLine() ?? String.Empty);
+
+
+        // for (int i = 0; i < length; i++)
+        // {
+            
+        // }
+        
+    }
+        
+
+
+
 }

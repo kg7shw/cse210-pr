9f27864b649adbe560de07ab880c1f6ee5b586fd
kg7shw <kg7shw@gmail.com>
2023-07-14 15:54:06 -0600

    fixed the json file format

--- final/FinalProject/Menu.cs
+++ final/FinalProject/Menu.cs
@@ -1,6 +1,10 @@
 public class Menu
 {
     public MealManager mealDriver { get; set; } = new MealManager();
+    public FileHandler fileHandler { get; set; } = new FileHandler();
+
+    public Meal meal { get; set; } = new Meal();
+
 
     public Menu()
     {
@@ -17,10 +21,11 @@ public class Menu
             Console.WriteLine("2. Make a meal");
             Console.WriteLine("3. Save recipes");
             Console.WriteLine("4. Load recipes");
-            Console.WriteLine("5. Quit");
+            Console.WriteLine("5. Display Recipes");
+            Console.WriteLine("6. Quit");
             Console.Write("Select a choice from the menu: ");
 
-            string choice = Console.ReadLine();
+            string choice = Console.ReadLine() ?? String.Empty;
 
             switch (choice)
             {
@@ -32,7 +37,7 @@ public class Menu
 
                 case "2":
                     mealDriver.MakeMeal();
-                    mealDriver.DisplayMeal();
+                    meal.DisplayMeal();
                     break;
 
                 case "3":
@@ -48,9 +53,32 @@ public class Menu
                     mealDriver.recipes = FileHandler.LoadRecipes(loadFileName);
                     mealDriver.recipeBook.SetRecipes(mealDriver.recipes);
                     Console.WriteLine("Recipes were loaded successfully.");
+                    Console.WriteLine("Recipes:");
+                    // mealDriver.recipeBook.DisplayRecipes();
                     break;
 
                 case "5":
+                    Console.WriteLine("1. List of Recipe names.");
+                    Console.WriteLine("2. List of Full Recipe.");
+                    Console.WriteLine("3. Return to the menu");
+                    string response = Console.ReadLine() ?? String.Empty;
+                    if (response.ToLower() == "1")
+                    {
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        mealDriver.recipeBook.DisplayRecipes();
+                        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                    }else if (response.ToLower() == "2")
+                    {   
+                        
+                        mealDriver.DisplayRecipes();
+                        
+                    } else
+                    {
+                    
+                    }
+                    break;
+
+                case "6":
                     quit = true;
                     Console.WriteLine("Have a nice day!");
                     break;
